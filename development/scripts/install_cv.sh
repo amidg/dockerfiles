308387b85b0ae1309ad6f13a306cb6f3a1958fdc
Dmitrii Gusev <amidg4x4dima@gmail.com>
2025-01-08 19:38:36 -0800

    initial CV commit

---
+++ development/scripts/install_cv.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# ################# #
+# Install GStreamer #
+# ################# #
+apt update -qqy && \
+apt install -qqy gstreamer1.0* \
+    ubuntu-restricted-extras
+    libgstreamer1.0-dev \
+    libgstreamer-plugins-base1.0-dev \
+    libgstreamer-plugins-bad1.0-dev
+
+# ################## #
+# Install OpenCV C++ #
+# ################## #
+# Install Dependencies
+apt install -qqy build-essential \
+    cmake \
+    git \
+    libgtk2.0-dev \
+    pkg-config \
+    libavcodec-dev \
+    libavformat-dev \
+    libswscale-dev \
+    python3-dev \
+    python3-numpy \
+    libtbb-dev \
+    libjpeg-dev \
+    libpng-dev \
+    libtiff-dev \
+    libdc1394-dev \
+    libgtk-3-dev \
+    libgtk2.0-dev
+
+# Build OpenCV C++
+git clone https://github.com/opencv/opencv.git /tmp/opencv
+cd /tmp/opencv
+git checkout $OPENCV_VERSION
